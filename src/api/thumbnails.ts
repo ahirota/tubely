21b5d84033d40c28eec18e22a375675d340a03c8
ahirota <avjhirota@gmail.com>
2026-02-26 12:33:20 -0800

    [CDN_1] File Storage Multipart Uploads

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -47,7 +47,38 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
 
-  return respondWithJSON(200, null);
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  const MAX_UPLOAD_SIZE = 10 << 20;
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Thumbnail file size must be < 10MB");
+  }
+
+  const type = file.type;
+  const buffer = await file.arrayBuffer();
+  const newThumbnail: Thumbnail = {
+    data: buffer,
+    mediaType: type
+  }
+
+  const video = await getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("You do not have access to edit this video");
+  }
+
+  videoThumbnails.set(video.id, newThumbnail);
+  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  video.thumbnailURL = thumbnailURL;
+  
+  await updateVideo(cfg.db, video);
+  return respondWithJSON(200, video);
 }
