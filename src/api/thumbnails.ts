ad8ffbddfe7b5e60138b54925186099760b578ad
ahirota <avjhirota@gmail.com>
2026-02-26 13:06:02 -0800

    [CDN_3] File Asset Save

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,6 +4,7 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { mediaTypeToExt, getAssetDiskPath, getAssetURL } from "./assets";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -16,25 +17,7 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  const formData = await req.formData();
-
-  const file = formData.get("thumbnail");
-  if (!(file instanceof File)) {
-    throw new BadRequestError("Thumbnail file missing");
-  }
-
-  const MAX_UPLOAD_SIZE = 10 << 20;
-
-  if (file.size > MAX_UPLOAD_SIZE) {
-    throw new BadRequestError("Thumbnail file size must be < 10MB");
-  }
-
-  const type = file.type;
-  const buffer = await file.arrayBuffer();
-  const base64Encoded = Buffer.from(buffer).toString("base64");
-  const dataURL = `data:${type};base64,${base64Encoded}`
-
-  const video = await getVideo(cfg.db, videoId);
+  const video = getVideo(cfg.db, videoId);
   if (!video) {
     throw new NotFoundError("Couldn't find video");
   }
@@ -42,7 +25,30 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new UserForbiddenError("You do not have access to edit this video");
   }
 
-  video.thumbnailURL = dataURL;
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Thumbnail file size must be < 10MB");
+  }
+
+  const type = file.type;
+  if (!type) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  const ext = mediaTypeToExt(type);
+  const filename = `${videoId}${ext}`;
+
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, file);
+
+  const assetURL = getAssetURL(cfg, filename);
+  video.thumbnailURL = assetURL;
 
   await updateVideo(cfg.db, video);
   return respondWithJSON(200, video);
